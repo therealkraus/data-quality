1adfbc524c6c4a22a2ed3b7cfb05e053b74ed207
Nikola Ilic <nikolailic25@live.ca>
2024-10-14 12:44:55 -0400

    initial code commit for dq profiler

---
+++ profiler/__main__.py
@@ -0,0 +1,14 @@
+# # Standard imports
+
+# Related third party imports
+import pandas as pd
+
+# Local application/library specific imports
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
